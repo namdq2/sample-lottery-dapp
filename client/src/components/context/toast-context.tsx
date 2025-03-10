663922f97d4203a2dfe3be7fa7f40cf14838eab4
Nam Dang <namquoc.dev@gmail.com>
2025-03-10 07:43:42 +0700

    add toast component

---
+++ client/src/components/context/toast-context.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React, { createContext, useState, useContext, ReactNode } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Toast, {
+  ToastProps,
+  ToastContainer,
+  ToastType,
+  ToastPosition,
+} from "../ui/toast";
+import { AnimatePresence } from "framer-motion";
+
+interface ToastContextProps {
+  showToast: (
+    message: string,
+    type: ToastType,
+    description?: string,
+    duration?: number
+  ) => void;
+  position: ToastPosition;
+  setPosition: (position: ToastPosition) => void;
+}
+
+const ToastContext = createContext<ToastContextProps>({
+  showToast: () => {},
+  position: "bottom-right",
+  setPosition: () => {},
+});
+
+export const ToastProvider = ({ children }: { children: ReactNode }) => {
+  const [toasts, setToasts] = useState<ToastProps[]>([]);
+  const [position, setPosition] = useState<ToastPosition>("bottom-right");
+
+  const showToast = (
+    message: string,
+    type: ToastType,
+    description?: string,
+    duration: number = 5000
+  ) => {
+    const id = uuidv4();
+    const newToast: ToastProps = {
+      id,
+      message,
+      type,
+      description,
+      duration,
+      onClose: (id) => {
+        setToasts((prevToasts) =>
+          prevToasts.filter((toast) => toast.id !== id)
+        );
+      },
+    };
+
+    setToasts((prevToasts) => [...prevToasts, newToast]);
+  };
+
+  return (
+    <ToastContext.Provider value={{ showToast, position, setPosition }}>
+      {children}
+      <div
+        className={`fixed ${
+          position === "top-right"
+            ? "top-4 right-4"
+            : position === "top-left"
+            ? "top-4 left-4"
+            : position === "bottom-left"
+            ? "bottom-4 left-4"
+            : "bottom-4 right-4"
+        } z-50`}
+      >
+        <AnimatePresence>
+          {toasts.map((toast) => (
+            <Toast key={toast.id} {...toast} />
+          ))}
+        </AnimatePresence>
+      </div>
+    </ToastContext.Provider>
+  );
+};
+
+export const useToast = () => useContext(ToastContext);
